3cb23081bc9baa8e7cfb9350655b11f42ebf02f5
Fisher Ankney <fisherankney@gmail.com>
2019-01-21 10:25:15 -0600

    employee database

---
+++ select_from.sql
@@ -0,0 +1,31 @@
+use sales_2;
+
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    first_name = 'John';
+	
+# command - B = beautify
+
+# SQL operators - and or in not in like not like exists not exists is null is not null
+
+# example
+
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    first_name = 'John' AND gender = 'M';
+    
+    
+# coding style
+    
+
+    
+    
+    
+    
+    
